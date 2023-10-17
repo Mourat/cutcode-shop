692bfdfda4ac2423eb115a16c7132dc22a93f938
Mourat <mourat@boutry.me>
2023-10-17 22:08:40 +0200

    chore:
    Added telegram notifications

--- config/logging.php
+++ config/logging.php
@@ -4,6 +4,7 @@ use Monolog\Handler\NullHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
 use Monolog\Processor\PsrLogMessageProcessor;
+use App\Logging\Telegram\TelegramLoggerFactory;
 
 return [
 
@@ -125,6 +126,14 @@ return [
 
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
+        ],
+
+        'telegram' => [
+            'driver' => 'custom',
+            'via' => TelegramLoggerFactory::class,
+            'level' => env('LOG_LEVEL', 'debug'),
+            'chat_id' => -4041233600,
+            'token' => '6523359748:AAG_SSSY5J2Kxc3V9TEeqf2LKmmRvUZeBq4'
         ],
     ],
 
